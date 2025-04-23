3162df7fca863c450a0aab531cfba92c2bf8a0b1
mint3382 <mskim0926@naver.com>
2025-04-23 17:30:47 +0900

    ⚙️ (setting) project 생성, sign in apple 적용

---
+++ Sedam/Sedam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sedam
+//
+//  Created by minsong kim on 4/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sedam/Sedam/SedamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SedamApp.swift
+//  Sedam
+//
+//  Created by minsong kim on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SedamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
