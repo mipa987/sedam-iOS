9733d0e08c5413967244ba28855e4f2b98f7df0c
mint3382 <mskim0926@naver.com>
2025-05-16 20:10:02 +0900

    🐞(fix) DecodedData 타입이 안 맞아도 http status 확인하도록 변경

--- Sedam/Sedam/Network/Manager/NetworkManager.swift
+++ Sedam/Sedam/Network/Manager/NetworkManager.swift
@@ -15,14 +15,13 @@ class NetworkManager {
         if let string = String(data: data, encoding: .utf8) {
             print("Response Body:", string)
         }
-        //Error
-        let decodedData: Builder.Response = try await builder.deserializer.deserialize(data)
         
         guard let httpResponse = response as? HTTPURLResponse else {
             throw NetworkError.responseNotFound
         }
         
         if (200...299).contains(httpResponse.statusCode) {
+            let decodedData: Builder.Response = try await builder.deserializer.deserialize(data)
             return decodedData
         } else if httpResponse.statusCode == 401 {
             throw NetworkError.accessDenied
