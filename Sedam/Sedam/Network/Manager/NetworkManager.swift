f2dbacf3568a165d252bd5eae56adfaaa135579c
mint3382 <mskim0926@naver.com>
2025-05-26 10:35:44 +0900

    🐞(fix) Refresh token 사용해 retry

--- Sedam/Sedam/Network/Manager/NetworkManager.swift
+++ Sedam/Sedam/Network/Manager/NetworkManager.swift
@@ -6,10 +6,11 @@
 //
 
 import Foundation
+import Supabase
 
 class NetworkManager {
     // MARK: - Public
-    func fetchData<Builder: BuilderProtocol>(_ builder: Builder) async throws -> Builder.Response {
+    func fetchData<Builder: BuilderProtocol>(_ builder: Builder, isRetry: Bool = false) async throws -> Builder.Response {
         let request = try await makeRequest(builder)
         let (data, response) = try await URLSession.shared.data(for: request)
         if let string = String(data: data, encoding: .utf8) {
@@ -20,13 +21,20 @@ class NetworkManager {
             throw NetworkError.responseNotFound
         }
         
-        if (200...299).contains(httpResponse.statusCode) {
-            let decodedData: Builder.Response = try await builder.deserializer.deserialize(data)
-            return decodedData
-        } else if httpResponse.statusCode == 401 {
+        switch httpResponse.statusCode {
+        case 200...299:
+            return try await builder.deserializer.deserialize(data)
+            
+        case 401 where !isRetry:
+            // 2) 토큰 갱신 시도
+            try await refreshToken()
+            // 3) 토큰 갱신 후, 재시도 플래그를 true로 해서 재요청
+            return try await fetchData(builder, isRetry: true)
+            
+        case 401:
             throw NetworkError.accessDenied
-        } else {
-            print(httpResponse.statusCode)
+            
+        default:
             throw NetworkError.invalidStatus(httpResponse.statusCode)
         }
     }
@@ -36,7 +44,7 @@ class NetworkManager {
         guard let baseURL = URL(string: builder.baseURL.rawValue) else {
             throw NetworkError.urlNotFound
         }
-
+        
         var url = baseURL.appendingPathComponent(builder.path)
         url.append(queryItems: builder.queries ?? [])
         
@@ -57,5 +65,17 @@ class NetworkManager {
         }
         
         return request
+    }
+    
+    private func refreshToken() async throws {
+        // (선택) 앱 포그라운드 진입 시 자동 갱신 루프 시작
+        await SupabaseManager.shared.supabase.auth.startAutoRefresh()
+        // 세션을 가져오기만 해도, 만료 시 리프레시가 이루어집니다
+        let session = try await SupabaseManager.shared.supabase.auth.session   // :contentReference[oaicite:1]{index=1}
+        
+        // 최신 토큰을 Keychain에 저장
+        KeyChainModule.create(key: .accessToken, data: session.accessToken)
+        KeyChainModule.create(key: .refreshToken, data: session.refreshToken)
+        print("✅Successfully refreshed token")
     }
 }
